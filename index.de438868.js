49b25fc6e44e6d9d0e9b6dc0e72caa4c6e8a21e5
Ivan Denys <denisivan06007@gmail.com>
2025-08-02 12:12:02 +0300

    make build

---
+++ index.de438868.js
@@ -0,0 +1,2 @@
+var e=document.querySelector(".wall"),t=e.querySelector(".spider"),c=e.clientWidth,l=e.clientHeight,i=t.clientWidth,n=t.clientHeight;t.style.top="".concat((c-i)/2,"px"),t.style.left="".concat((l-n)/2,"px");
+//# sourceMappingURL=index.de438868.js.map
